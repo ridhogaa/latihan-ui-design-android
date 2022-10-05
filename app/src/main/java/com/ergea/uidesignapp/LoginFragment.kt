831e2e5dd2abee081e5ce2e42aab838339f4be4e
ridhogaa <ridhoga186@gmail.com>
2022-10-05 22:02:21 +0700

    first commit UI

---
+++ app/src/main/java/com/ergea/uidesignapp/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.ergea.uidesignapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.ergea.uidesignapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnLogin.setOnClickListener {
+            it.findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
